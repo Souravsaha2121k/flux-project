2e559ba306517c17c0cf4176be1138ffbd259915
Souravsaha2121k <65421415+Souravsaha2121k@users.noreply.github.com>
2020-05-16 20:00:21 +0530

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,40 @@
+'''
+Tests for jwt flask app.
+'''
+import os
+import json
+import pytest
+
+import main
+
+SECRET = 'TestSecret'
+TOKEN = "17e0ad6c91a23c2e62db1b2fad83b964e8bb4ac7"
+EMAIL = 'wolf@thedoor.com'
+PASSWORD = 'huff-puff'
+
+@pytest.fixture
+def client():
+    os.environ['JWT_SECRET'] = SECRET
+    main.APP.config['TESTING'] = True
+    client = main.APP.test_client()
+
+    yield client
+
+
+
+def test_health(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json == 'Healthy'
+
+
+def test_auth(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
